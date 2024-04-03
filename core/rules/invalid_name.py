7292f30eb02acd11877a26da87c8c28022cfbf39
Agustín Munita <amunitac@uc.cl>
2024-04-03 16:23:05 -0300

    feat: invalid name visitor ready

--- core/rules/invalid_name.py
+++ core/rules/invalid_name.py
@@ -4,14 +4,23 @@ import re
 
 class InvalidNameVisitor(WarningNodeVisitor):
 
+    def __init__(self):
+        super().__init__()
+        self.inClass = False
+
     def visit_ClassDef(self, node: ClassDef):
+        self.inClass = True
         if not re.match(r'^[A-Z][a-zA-Z0-9]+$', node.name):
             self.addWarning('InvalidName', node.lineno, 'invalid class name ' + node.name)
         NodeVisitor.generic_visit(self, node)
+        self.inClass = False
 
     def visit_FunctionDef(self, node: FunctionDef):
-        if not re.match(r'^[a-z][a-z0-9_]{2,30}$', node.name):
+        if not re.match(r'^[a-z][a-z0-9_]{2,30}$', node.name) and not self.inClass:
             self.addWarning('InvalidName', node.lineno, 'invalid function name ' + node.name)
+        else:
+            if not re.match(r'^[a-z][a-z0-9_]{2,30}$', node.name) and self.inClass:
+                self.addWarning('InvalidName', node.lineno, 'invalid method name ' + node.name)
         NodeVisitor.generic_visit(self, node)
         
 
