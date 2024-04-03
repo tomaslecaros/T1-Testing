2815ab1ea76dc118f104b0ca201d03325865869c
Agustín Munita <amunitac@uc.cl>
2024-04-03 17:30:56 -0300

    feat: simplifiable if visitor ready

--- core/rules/simplifiable_if.py
+++ core/rules/simplifiable_if.py
@@ -1,3 +1,4 @@
+from typing import Any
 from ..rule import *
 
 # Clases que permiten detectar el uso de if statements o if ternarios que 
@@ -8,10 +9,31 @@ class SimplifiableIfVisitor(WarningNodeVisitor):
     def __init__(self):
         super().__init__()
 
+    def visit_IfExp(self, node: IfExp):
+        if isinstance(node.body, Constant):
+            if node.body.value == True or node.body.value == False:
+                self.addWarning('SimplifiableIf', node.lineno, 'if statement can be replaced with a bool(test)')
+        elif isinstance(node.orelse, Constant):
+            if node.orelse.value == True or node.orelse.value == False:
+                self.addWarning('SimplifiableIf', node.lineno, 'if statement can be replaced with a bool(test)')
+        NodeVisitor.generic_visit(self, node)
+
+    def visit_If(self, node: If):
+        for item in node.body:
+            if isinstance(item, Return):
+                if isinstance(item.value, Constant):
+                    if item.value.value == True or item.value.value == False:
+                        self.addWarning('SimplifiableIf', node.lineno, 'if statement can be replaced with a bool(test)')
+            if isinstance(item, Assign):
+                if isinstance(item.value, Constant):
+                    if item.value.value == True or item.value.value == False:
+                        self.addWarning('SimplifiableIf', node.lineno, 'if statement can be replaced with a bool(test)')        
 
 class SimplifiableIfRule(Rule):
     def analyze(self, ast):
-        pass
+        visitor = SimplifiableIfVisitor()
+        visitor.visit(ast)
+        return visitor.warningsList()
     
     @classmethod
     def name(cls):
