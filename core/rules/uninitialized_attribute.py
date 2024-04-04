40c9fed3eac482d290bd2dcc95261293797f0a71
Agustín Munita <amunitac@uc.cl>
2024-04-04 14:48:59 -0300

    feat: uninitialized attribute ready

--- core/rules/uninitialized_attribute.py
+++ core/rules/uninitialized_attribute.py
@@ -7,12 +7,28 @@ class UninitializedAttributeVisitor(WarningNodeVisitor):
 
     def __init__(self):
         super().__init__()
+        self.initialized_attributes = set()
+
+    def visit_ClassDef(self, node):
+        for stmt in node.body:
+            if isinstance(stmt, FunctionDef) and stmt.name == '__init__':
+                for subnode in stmt.body:
+                    if isinstance(subnode, Assign):
+                        if isinstance(subnode.targets[0], Attribute):
+                            self.initialized_attributes.add(subnode.targets[0].attr)
+        NodeVisitor.generic_visit(self, node)
+
+    def visit_Attribute(self, node):
+        if isinstance(node.ctx, Load) and node.attr not in self.initialized_attributes:
+            self.addWarning('UninitializedAttribute', node.lineno, f'{node.attr} attribute was not initialized!')
 
 
 class UninitializedAttributeRule(Rule):
 
     def analyze(self, ast):
-        pass
+        visitor = UninitializedAttributeVisitor()
+        visitor.visit(ast)
+        return visitor.warnings
 
     @classmethod
     def name(cls):
