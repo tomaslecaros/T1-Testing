e5ec8f06c73840a55aed63ec0bdb554c62f71195
Agustín Munita <amunitac@uc.cl>
2024-04-09 10:45:23 -0400

    feat: operator_equals_rewriter transformer done

--- core/transformers/operator_equals_rewriter.py
+++ core/transformers/operator_equals_rewriter.py
@@ -8,8 +8,35 @@ class OperatorEqualsTransformer(NodeTransformer):
     def __init__(self):
         super().__init__()
 
+    def visit_Assign(self, node):
+        NodeTransformer.generic_visit(self, node)
+        if isinstance(node.value, BinOp):
+            left = node.targets[0]
+            binOp = node.value
+            op = binOp.op
+            if left.id == binOp.left.id:
+                right = binOp.right
+            else:
+                right = binOp.left
+            if isinstance(op, Add):
+                node = AugAssign(target=left, op=Add(), value=right)
+            elif isinstance(op, Sub):
+                node = AugAssign(target=left, op=Sub(), value=right)
+            elif isinstance(op, Mult):
+                node = AugAssign(target=left, op=Mult(), value=right)
+            elif isinstance(op, Div):
+                node = AugAssign(target=left, op=Div(), value=right)
+            elif isinstance(op, Mod):
+                node = AugAssign(target=left, op=Mod(), value=right)
+            elif isinstance(op, Pow):
+                node = AugAssign(target=left, op=Pow(), value=right)
+            elif isinstance(op, FloorDiv):
+                node = AugAssign(target=left, op=FloorDiv(), value=right)
+
+        return node
 
 class OperatorEqualsCommand(RewriterCommand):
 
     def apply(self, ast):
-        pass
+        new_tree = fix_missing_locations(OperatorEqualsTransformer().visit(ast))
+        return new_tree
